75bcdd62c4e1f63e4f8fcd8b881552948da151c7
Santosh Gaikar <santoshgaikar1@gmail.com>
2020-07-30 17:45:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +18,38 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+       <div id="navbar">
+        <a href="#" id="gmail">Gmail</a>
+        <a href="#" id="images">Images</a>
+        <a href="#" id="menu"><img src="https://cdn4.iconfinder.com/data/icons/finite-application/16/grid-9-512.png" width="20px" height="20px"></img></a>
+        <a href="#" id="signin">Sign In</a>
+    </div>
+
+    <div id="main">
+        <a href="#" id="logo"><img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width="300px" height="100px"></img></a>
+        <input type="text" id="search"></input>
+        <div id="flexbox">
+        <button id="google-search-btn" onClick={getInputValue}>Google Search</button>
+        <button id="random-btn">I'm Feeling Lucky</button>   
+        </div>
+        <p id="google-text">Google offered in: <a href="#" id="english">English</a></p>     
+        <a href="#" id="output">Your entered value is: </a>
+    </div>
+
+    <div id="footer">
+        <a href="#" id="privacy">Privacy</a>
+        <a href="#" id="terms">Terms</a>
+        <a href="#" id="settings">Settings</a>
+    </div>
+
     </div>
   );
+  function getInputValue(){
+    var inputVal = document.getElementById("search").value;
+    document.getElementById("output").innerHTML = "Your entered value is: "+inputVal
+    window.open("https://www.google.com/?query="+inputVal)
+}
 }
 
 export default App;
